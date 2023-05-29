fdb80ff9fa1a77561de17d43a46f0ad6dd755d12
keybow <keybow007@gmail.com>
2023-05-29 16:50:04 +0900

    add comments

--- lib/src/web_url_strategy_interface.dart
+++ lib/src/web_url_strategy_interface.dart
@@ -3,13 +3,11 @@ import 'package:web_url_strategy/src/web_url_strategy_stub.dart'
     if (dart.library.io) 'package:web_url_strategy/src/impl/web_url_strategy_impl_mobile.dart';
 
 
-//１．抽象クラス（インターフェース）の作成
 //  Create an abstract class to define the methods you will need to use.
 abstract class WebUrlStrategy {
   void setPathUrlStrategy() {}
 
   void setHashUrlStrategy() {}
 
-  //１の段階ではgetUrlStrategyはニョロニョロOK（プラットフォーム毎の実装ファイルが未作成のため）
   factory WebUrlStrategy() => getWebUrlStrategy();
 }
--- lib/web_url_strategy.dart
+++ lib/web_url_strategy.dart
@@ -1,3 +1,16 @@
+/*
+* Reference for implementation
+*   url_strategy package
+*   https://github.com/simpleclub/url_strategy/tree/main/url_strategy/lib
+*
+*   How to import platform specific dependency in Flutter/Dart? (Combine Web with Android/iOS)
+*   https://stackoverflow.com/a/58713064/7300575
+*
+*   Configuring the URL strategy on the web
+*   https://docs.flutter.dev/ui/navigation/url-strategies
+* */
+
+
 library web_url_strategy;
 
 export 'src/web_url_strategy_interface.dart';
