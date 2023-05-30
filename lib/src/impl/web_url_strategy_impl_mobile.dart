d07152d57217bb1a19623af0a4c477bdb877926f
keybow <keybow007@gmail.com>
2023-05-30 12:54:53 +0900

    remove unnecessary comments

--- lib/src/impl/web_url_strategy_impl_mobile.dart
+++ lib/src/impl/web_url_strategy_impl_mobile.dart
@@ -1,7 +1,3 @@
-//２−２．Mobileの場合の実装クラス作成（空実装でいい）
-//https://github.com/simpleclub/url_strategy/blob/main/url_strategy/lib/src/url_strategy_web.dart
-
-
 import 'package:web_url_strategy/src/web_url_strategy_interface.dart';
 
 class UrlStrategyImplMobile implements WebUrlStrategy {
--- lib/src/impl/web_url_strategy_impl_web.dart
+++ lib/src/impl/web_url_strategy_impl_web.dart
@@ -1,10 +1,8 @@
-//「as flutterWeb」をつけないと自作成の「UrlStrategy」クラスがimportできない
 import 'package:flutter_web_plugins/flutter_web_plugins.dart' as flutterweb;
 
 import '../web_url_strategy_interface.dart';
 
-//２−１．Webの場合の実装クラス作成
-//https://github.com/simpleclub/url_strategy/blob/main/url_strategy/lib/src/url_strategy_web.dart
+
 class UrlStrategyImplWeb implements WebUrlStrategy {
   @override
   void setHashUrlStrategy() {
